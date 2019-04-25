64c2da22a25f21a072b20d378ac17677730bc7e2
往逸 <1159294999@qq.com>
2019-04-25 23:42:55 +0800

    auth loading style fix

--- src/auth/loading.js
+++ src/auth/loading.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ActivityIndicator, View, AsyncStorage } from 'react-native';
+import { _primary } from '../constants/style';
 
 export default class AuthLoadingScreen extends React.Component {
   constructor(props) {
@@ -14,8 +15,13 @@ export default class AuthLoadingScreen extends React.Component {
 
   render() {
     return (
-      <View>
-        <ActivityIndicator />
+      <View style={{
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+        <ActivityIndicator color={_primary} />
       </View>
     );
   }
